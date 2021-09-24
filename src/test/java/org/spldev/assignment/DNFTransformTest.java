4593c260cee345b60961d8f861342f35b88fb632
Sebastian Krieter <sebastian.krieter@ovgu.de>
2021-09-24 14:08:26 +0200

    Added copyright text to some classes.

--- src/test/java/org/spldev/assignment/DNFTransformTest.java
+++ src/test/java/org/spldev/assignment/DNFTransformTest.java
@@ -1,3 +1,25 @@
+/* -----------------------------------------------------------------------------
+ * Formula-Analysis Lib - Library to analyze propositional formulas.
+ * Copyright (C) 2021  Sebastian Krieter
+ * 
+ * This file is part of Formula-Analysis Lib.
+ * 
+ * Formula-Analysis Lib is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Formula-Analysis Lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Formula-Analysis Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * See <https://github.com/skrieter/formula-analysis> for further information.
+ * -----------------------------------------------------------------------------
+ */
 package org.spldev.assignment;
 
 import static org.junit.jupiter.api.Assertions.*;
